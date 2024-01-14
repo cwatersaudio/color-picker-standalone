1f7e322a62d8de2f9e169e30c35d87c40a03eed7
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2024-01-13 23:32:23 -0500

    finished basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,19 @@ import Navbar from "./components/Navbar";
 import ColorSwatch from "./components/ColorSwatch";
 
 function App() {
+  const testColors = ["red", "yellow", "green", "blue", "purple"];
+  const colorSwatches = testColors.map((item) => {
+    return <ColorSwatch color={item} />;
+  });
+
+  console.log(colorSwatches);
+
   return (
     <>
       <Navbar />
-      <ColorSwatch />
+      <div className='swatch--container'>{colorSwatches}</div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/ColorSwatch.jsx
+++ src/components/ColorSwatch.jsx
@@ -3,9 +3,15 @@ import React from "react";
 export default function ColorSwatch(props) {
   return (
     <>
-      <div className='color--block'>
-        <div className='hex--code'>
-          <span className='hex--code'>{props.hex}</span>
+      <div className='swatch'>
+        <div
+          className='color--block'
+          style={{ backgroundColor: props.color }}
+        ></div>
+        <div className='hex--code--container'>
+          <div className='hex--code'>
+            <span>#1234</span>
+          </div>
         </div>
       </div>
     </>
