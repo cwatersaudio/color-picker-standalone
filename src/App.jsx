4aadb442675b2165562369899823b3e01c173871
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2024-01-15 12:32:12 -0500

    refactored to skip primary bar; only 4 colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import ColorSwatch from "./components/ColorSwatch";
-import PrimaryBar from "./components/PrimaryBar";
 
 function App() {
   const [colors, setColors] = React.useState({
@@ -16,7 +15,7 @@ function App() {
     ///scheme{?hex,rgb,hsl,cmyk,format,mode,count
     const trimmedHex = colors.seedColor.slice(1);
     fetch(
-      `https://www.thecolorapi.com/scheme?hex=${trimmedHex}&format=json&mode=analogic-complement&count=5`
+      `https://www.thecolorapi.com/scheme?hex=${trimmedHex}&format=json&mode=analogic-complement&count=4`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -24,8 +23,8 @@ function App() {
         const colorSwatchAPI = data.colors.map((item) => {
           return item.hex.value;
         });
-        console.log(colorSwatchAPI);
-        updateColorScheme(colorSwatchAPI);
+        const newSwatch = [colors.seedColor,...colorSwatchAPI]
+        updateColorScheme(newSwatch);
       });
   }, [colors.seedColor]);
 
@@ -51,16 +50,16 @@ function App() {
   function updateTempColor(event) {
     setTempSeed(event.target.value);
   }
-  const testColors = ["red", "yellow", "green", "blue", "purple"];
   const colorSwatches = colors.swatches.map((item) => {
-    return <ColorSwatch color={item} />; //will eventually send hex
+    return <ColorSwatch  color={item} />; //will eventually send hex
   });
   return (
     <>
     <main>
       <Navbar colors={colors} updateSeedColor={updateSeedColor} updateTempColor={updateTempColor} tempSeed = {tempSeed} />
-      <PrimaryBar primaryColor={colors.seedColor} />
-      <div className='swatch--container'>{colorSwatches}</div>
+      <div className='swatch--container'>
+        {colorSwatches}
+      </div>
     </main>
     </>
   );
--- src/components/PrimaryBar.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default function PrimaryBar(props) {
-
-    return(
-        <div className='primary--bar' style={{backgroundColor: props.primaryColor}}></div>
-    )
-}
